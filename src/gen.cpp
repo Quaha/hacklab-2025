c9c3ef153572ac082ec137519df2190bb9b44091
Daniil <darenoheg@gmail.com>
2025-11-16 15:56:43 +0300

    fix

--- src/gen.cpp
+++ src/gen.cpp
@@ -3,7 +3,7 @@
 uint64_t Generator::a = 16807ull;
 uint64_t Generator::mod = 2147483647ull;
 
-void initDegs(uint32_t degs[32], uint64_t a, uint64_t mod) {
+void initDegs(uint64_t degs[32], uint64_t a, uint64_t mod) {
 
 	uint64_t value = 1ull;
 	for (int i = 0; i < 32; i++) {
@@ -57,4 +57,47 @@ void Generator::move(int shift) {
 
 	state *= shift_value;
 	state %= mod;
+}
+
+VGenerator::VGenerator(uint64_t seed, int shift) {
+	uint32_t cstate = seed % mod;
+
+	uint64_t value = a;
+	for (int i = 0; i < 32; i++) {
+		if (shift & (1 << i)) {
+			cstate *= value;
+			cstate %= mod;
+		}
+		value *= value;
+		value %= mod;
+	}
+
+	uint64_t v = 1ull;
+
+	for (int i = 0; i < VGENSZ; i++) {
+		degs[i] = v;
+		v *= a;
+		v %= mod;
+	}
+
+	shift_par = v;
+
+	for (int i = 0; i < VGENSZ; i++) {
+		state[i] = (cstate * degs[i]) % mod;
+	}
+
+
+}
+
+void VGenerator::copy(int ci, int otheri, uint32_t* data) {
+	for (int i = 0; i < ci; i++) {
+		data[otheri + i] = state[ci];
+	}
+}
+
+void VGenerator::change() {
+	for (int i = 0; i < VGENSZ; ++i) {
+		state[i] *= shift_par;
+		state[i] %= mod;
+	}
 }
